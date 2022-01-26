83012d95455ca2b29d601b596494cd3997263a41
Rafaela Lima Nogueira <rn-15@hotmail.com>
2022-01-26 10:43:10 -0300

    Validando a entrada de numeros

--- adivinhacao 2.py
+++ adivinhacao 2.py
@@ -7,9 +7,14 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {} tentativas".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
 
     chute = int(chute_str)
+    
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+    
     acertou = numero_secreto == chute
     menor = chute > numero_secreto
     maior = chute < numero_secreto
